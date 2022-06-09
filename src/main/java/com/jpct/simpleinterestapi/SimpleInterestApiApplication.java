e24b6eb9afec560e9859f86f5ccefb3d6d94091f
JPCT <juan.pablo.999@outlook.com>
2022-06-09 08:11:15 -0500

    Swagger documentation

--- src/main/java/com/jpct/simpleinterestapi/SimpleInterestApiApplication.java
+++ src/main/java/com/jpct/simpleinterestapi/SimpleInterestApiApplication.java
@@ -1,7 +1,11 @@
 package com.jpct.simpleinterestapi;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleInterestApiApplication {
@@ -10,4 +14,11 @@ public class SimpleInterestApiApplication {
         SpringApplication.run(SimpleInterestApiApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().components(new Components())
+                .info(new Info().title("Simple interest API")
+                        .description("API microservice to calculate simple interest")
+                        .version("1.0"));
+    }
 }
